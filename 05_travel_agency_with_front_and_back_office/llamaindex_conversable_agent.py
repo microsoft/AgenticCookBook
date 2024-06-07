ea9c242f481e079cc5a238d229fa980139411ad4
Diego Colombo <dicolomb@microsoft.com>
2024-06-07 17:57:05 +0100

    Add copyright and license information to main.py and llamaindex_conversable_agent.py
    This commit adds the copyright notice and license information to the main.py and llamaindex_conversable_agent.py files. This ensures that the code is properly licensed under the MIT license.

--- 05_travel_agency_with_front_and_back_office/llamaindex_conversable_agent.py
+++ 05_travel_agency_with_front_and_back_office/llamaindex_conversable_agent.py
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 from autogen import OpenAIWrapper
 from autogen.agentchat import Agent, ConversableAgent
 from autogen.agentchat.contrib.vectordb.utils import get_logger
